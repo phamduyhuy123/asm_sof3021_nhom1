fb00426b661492a499b0a6c8daa090e62d58c5ce
tieuuu <phamhuytrieu12092003@gmail.com>
2023-06-05 15:36:03 +0700

    them CRUD vao trang product

--- src/main/java/com/nhom2/asmsof3021/controller/productController/ProductController.java
+++ src/main/java/com/nhom2/asmsof3021/controller/productController/ProductController.java
@@ -1,9 +1,11 @@
 package com.nhom2.asmsof3021.controller.productController;
 
+import com.nhom2.asmsof3021.controller.AdminPageController;
 import com.nhom2.asmsof3021.model.Category;
 import com.nhom2.asmsof3021.model.Laptop;
 import com.nhom2.asmsof3021.model.Product;
 import com.nhom2.asmsof3021.repository.CategoryRepo;
+import com.nhom2.asmsof3021.repository.productRepo.ProductRepo;
 import com.nhom2.asmsof3021.security.UserRepository;
 import com.nhom2.asmsof3021.service.CategoryService;
 import com.nhom2.asmsof3021.service.ProductService;
@@ -13,9 +15,15 @@ import jakarta.persistence.metamodel.EntityType;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.nhom2.asmsof3021.utils.AuthenticateUtil.checkIsAuthenticated;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,6 +32,8 @@ public class ProductController  {
     private final HttpSession session;
     @PersistenceContext
     private final EntityManager entityManager;
+    private final ProductRepo productRepo;
+    private final UserRepository userRepository;
     @GetMapping("/admin/product/generate-input-fields/{categoryId}")
     @ResponseBody
     public String generateInputFieldProductDependOnCategory(@PathVariable Integer categoryId){
@@ -103,5 +113,24 @@ public class ProductController  {
         return category.getEntityClassName();
     }
 
+    @GetMapping("/admin/product/EDIT/{id}")
+    public String edit(@PathVariable Integer id, Model model, Principal principal){
+        checkIsAuthenticated(principal,session,userRepository);
+        List<Category> categoryList=categoryRepository.findAllByEntityClassNameExists();
+        model.addAttribute("categories" ,categoryList);
+        List<AdminPageController.BreadcrumbLink> breadcrumbLinkList=new ArrayList<>();
+        breadcrumbLinkList.add(new AdminPageController.BreadcrumbLink("Product","/admin/product"));
+        breadcrumbLinkList.add(new AdminPageController.BreadcrumbLink("Product Management","/admin/product/management"));
+        session.setAttribute("breadcrumbs", breadcrumbLinkList);
 
+        Product product = productRepo.findById(id).orElseThrow();
+        model.addAttribute("product",product);
+        List<Product> list = productRepo.findAll();
+        model.addAttribute("products",list);
+        return "admin/productManagement";
+    }
+//    public String delete(){
+//
+//        return
+//    }
 }
--- src/main/java/com/nhom2/asmsof3021/controller/productController/admin/LaptopController.java
+++ src/main/java/com/nhom2/asmsof3021/controller/productController/admin/LaptopController.java
@@ -1,21 +1,47 @@
 package com.nhom2.asmsof3021.controller.productController.admin;
 
+import com.nhom2.asmsof3021.controller.productController.CrudMvcMethod;
 import com.nhom2.asmsof3021.model.Laptop;
+import com.nhom2.asmsof3021.model.Monitor;
 import com.nhom2.asmsof3021.repository.productRepo.LaptopRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/product/management/Laptop")
-public class LaptopController {
+public class LaptopController extends CrudMvcMethod<Laptop, Integer> {
     private final LaptopRepo repo;
-    @PostMapping("/create")
-    public String create(@ModelAttribute Laptop laptop){
 
-        return "admin/productManagement";
+    @Override
+    @PostMapping("/create")
+    @ResponseBody
+    public String create(@ModelAttribute Laptop laptop, RedirectAttributes redirectAttributes){
+        repo.save(laptop);
+        return "/admin/product/management";
+    }
+
+    @Override
+    @DeleteMapping("/DELETE/{id}")
+    @ResponseBody
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Laptop laptop=repo.findById(id).orElseThrow();
+        if(laptop!=null){
+            repo.delete(laptop);
+        }
+        return "/admin/product/management";
+    }
+
+    @Override
+    @PutMapping("/UPDATE/{id}")
+    @ResponseBody
+    public String update(Laptop laptop, @PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        if(repo.findById(id).isPresent()){
+            repo.save(laptop);
+            redirectAttributes.addFlashAttribute("statusMessage","Success");
+        }
+        return "/admin/product/management";
     }
 }
